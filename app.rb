8193982542ffec71bd9123498a1522b15c6fcd52
Tom Barnes <ruralmyth@gmail.com>
2015-10-26 16:47:59 +0000

    added index.erb

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
-  </div>"
+  erb(:index)
 end
 
 get '/secret' do
